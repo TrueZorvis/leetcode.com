9b4d3ca546ed027f4fb55909e9af3bb82aee9f18
rg.batyrshin <rg.batyrshin@localhost>
2024-01-21 15:13:48 +0600

    36, 238

---
+++ python/medium/0036. Valid Sudoku.py
@@ -0,0 +1,108 @@
+"""
+Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+Each row must contain the digits 1-9 without repetition.
+Each column must contain the digits 1-9 without repetition.
+Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+Note:
+A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+Only the filled cells need to be validated according to the mentioned rules.
+
+Example 1:
+Input: board =
+[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: true
+
+Example 2:
+Input: board =
+[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]
+Output: false
+
+Explanation: Same as Example 1, except with the 5 in the top left corner being modified to 8.
+Since there are two 8's in the top left 3x3 sub-box, it is invalid.
+
+Constraints:
+board.length == 9
+board[i].length == 9
+board[i][j] is a digit 1-9 or '.'.
+"""
+from collections import defaultdict
+
+
+class Solution:
+    def isValidSudoku(self, board: list[list[str]]) -> bool:
+        cols = dict()
+        rows = dict()
+        boxes = dict()
+
+        for idx_row, row in enumerate(board):
+            for idx_col, el in enumerate(row):
+                if el == '.':
+                    continue
+
+                idx_box = (idx_row // 3, idx_col // 3)
+                if idx_col not in cols:
+                    cols[idx_col] = set()
+                if idx_row not in rows:
+                    rows[idx_row] = set()
+                if idx_box not in boxes:
+                    boxes[idx_box] = set()
+
+                if (
+                        el in cols[idx_col] or
+                        el in rows[idx_row] or
+                        el in boxes[idx_box]
+                ):
+                    return False
+
+                cols[idx_col].add(el)
+                rows[idx_row].add(el)
+                boxes[idx_box].add(el)
+
+        return True
+
+
+s = Solution()
+board = [
+    ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+]
+res = s.isValidSudoku(board)
+print(res)
+
+board = [
+    ["8", "3", ".", ".", "7", ".", ".", ".", "."],
+    ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", "6", "."],
+    ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+    ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", "6", ".", ".", ".", ".", "2", "8", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+]
+res = s.isValidSudoku(board)
+print(res)
